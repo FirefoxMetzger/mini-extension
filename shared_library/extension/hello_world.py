6f4d1f25d7d6fe0bb76f1dbfb5de7acb7fdd3b12
Sebastian Wallkötter <sebastian@wallkoetter.net>
2022-07-24 08:36:38 +0200

    better docs

--- shared_library/extension/hello_world.py
+++ shared_library/extension/hello_world.py
@@ -1,8 +1,11 @@
 from ctypes import cdll, c_char_p
 from pathlib import Path
 
+# load the library using ctypes
 library_location = Path(__file__).parent.absolute() / "lib" / "hello_library"
 lib_shared_hello = cdll.LoadLibrary(library_location.as_posix())
 
+# expose the hello function as hello_world in python
+# Note: returns a bytes object, not a python string
 hello_world = lib_shared_hello.hello
 hello_world.restype = c_char_p
