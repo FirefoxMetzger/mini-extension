283a5c87c51f4799df38c18f9cd31cb5f7f57398
Gabriele Pongelli <gpongelli@users.noreply.github.com>
2023-01-14 15:50:58 +0100

    Os independent feature (#2)
    Co-authored-by: FirefoxMetzger <sebastian@wallkoetter.net>

--- shared_library_with_ctypes/extension/hello_world.py
+++ shared_library_with_ctypes/extension/hello_world.py
@@ -1,11 +1,35 @@
+import platform
+import os
+import re
+
 from ctypes import cdll, c_char_p
 from pathlib import Path
 
 # load the library using ctypes
-library_location = Path(__file__).parent.absolute() / "lib" / "hello_library"
-lib_shared_hello = cdll.LoadLibrary(library_location.as_posix())
+_extension = ''
+if platform.system() == ' Windows':
+    _extension = 'dll'
+elif platform.system() == 'Linux':
+    _extension = 'so'
+
+_base_path = Path(__file__).parent.absolute().joinpath('lib')
+# linux do put 'lib' as name prefix and can put the version after the extension (e.g. libhello_library.so.0.1.0 )
+res = [f for f in os.listdir(_base_path) if re.search(rf'[a-z]*hello_library[a_z]*.{_extension}[.0-9]*', f)]
+
+# just one library should be into the folder, otherwise exception is raised
+lib_shared_hello = cdll.LoadLibrary((_base_path / res[0]).as_posix())
 
 # expose the hello function as hello_world in python
-# Note: returns a bytes object, not a python string
-hello_world = lib_shared_hello.hello
-hello_world.restype = c_char_p
+_hello_world = lib_shared_hello.hello
+_hello_world.restype = c_char_p
+
+
+def hello_world():
+    """Prints a hello world message.
+
+    In ctypes, a C string is given to python as a `bytes()` object. This wrapper
+    converts it into the expected string object.
+    
+    """
+
+    return _hello_world().decode()
